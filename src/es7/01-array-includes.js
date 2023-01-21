dcfa91f5908f09a60915bc66715a80c5587d53a3
Daniel Dominguez Dominguez <danarturodo@hotmail.com>
2023-01-20 22:49:23 -0500

    Pasamos a ES 8 :D

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,7 @@
+let numbers = [1,3,4,6,7,8];
+console.log(numbers.includes(0)); //Detectamos con un boolean si el numero esta en el array numbers
+
+const list = ['Freddy','Bob','Esponja'];
+console.log(list.includes('Bob')); //Detectamos con un boleaan si el string es o no!
+
+
